5f6938832ac443ef63572a4575267efc2f2cb890
Mark Olson <mlo@baskerville.org>
2019-02-02 14:34:40 -0500

    Add code to handle tables with spanned columns

--- TableConverter.py
+++ TableConverter.py
@@ -79,16 +79,6 @@ def ProcessFile(filename):
 
 
 #=============================================================
-# Check for advanced table features (like merged cells) which the MediaWiki SimpleTable can't handle.
-def CheckForAdvancedTable(table):
-    for tline in table:
-        if "||||" in tline:
-            # Log error message
-            return True
-    return False
-
-
-#=============================================================
 # Check for Fancy table: Is this table a quote from Fancy 1 or Fancy 2?
 # If 'yes' return "fancy1" or "facny2".  If 'no', return None
 def CheckForFancyTable(table):
@@ -116,57 +106,25 @@ def CheckForFancyTable(table):
 
 
 #=============================================================
-# Process a single table
-# There are three kinds of tables:
-#   Tables with merged cells
-#   Fancy1 and Facy 2 quotes
-#   Ordinary tables (no merged cells, no "Fancyclopedia 1" or "Fancyclopedia 1" in the first line).  Ordinary tables may or may not have a header row
-def ProcessTable(table):
-
-    # First, check to make sure there are no runs of more than two "|" -- that would indicate a table that can't be handled by
-    # the SimpleTable thingie.
-    if CheckForAdvancedTable(table):
-        print("this is an advanced table")
-        return table
-
-    out=[]
-    # The second case is a Fancy 1 or Fancy 2 quote table
-    fancy=CheckForFancyTable(table)
-    if fancy is not None:
-        out.append("{{"+fancy+"|text=")
-        for line in table[1:]:      # We skip the 1st line
-            out.append(GenerateNewTableLine(AnalyzeTableLine(line)))
-        out.append("}}")
-    else:
-        # Generate the output table's first line.
-        # If the input table contains "||~" it's a headerline and will need some special handing in the output.
-        header=AnalyzeTableLine(table[0])
-        headerline="<tab class=wikitable sep=barbar "
-        if "||~" in table[0]:
-            headerline+="head=top "
-        else:
-            headerline+="head= "
-        headerline+="border=1>\n"
-
-        if "||~" in table[0]:
-            if header is not None:
-                for head in header:
-                    headerline+=head+"||"
-                headerline=headerline[:-1]    # We drop the last "|"
-                lines=table[1:]     # If we consume the header line here, remove it.
-        out.append(headerline)
-
-        # Now process the rest of the lines
-        for line in table:
-            out.append(GenerateNewTableLine(AnalyzeTableLine(line)))
-        out.append("</tab>")
-
-    return out
+# Convert Wikidot's column spanning codes to SimpleTable's
+def ConvertColspans(line):
+    line=line.replace("||||||||||||~", '||~ colspan="6"|')
+    line=line.replace("||||||||||||", '|| colspan="6"|')
+    line=line.replace("||||||||||", '||~ colspan="5"|')
+    line=line.replace("||||||||||", '|| colspan="5"|')
+    line=line.replace("||||||||~", '||~ colspan="4"|')
+    line=line.replace("||||||||", '|| colspan="4"|')
+    line=line.replace("||||||~", '||~ colspan="3"|')
+    line=line.replace("||||||", '|| colspan="3"|')
+    line=line.replace("||||~", '||~ colspan="2"|')
+    line=line.replace("||||", '|| colspan="2"|')
+    return line
 
 
 #=============================================================
 # Given a complete line in a table, split it into individual cells.
 def AnalyzeTableLine(line):
+    line=ConvertColspans(line)
     if "||~" in line:
         # The header line delimiters can be either "||" or "||~"
         line=line.replace("||~", "||")
@@ -196,6 +154,53 @@ def GenerateNewTableLine(line):
     return newline
 
 
+#=============================================================
+# Process a single table
+# There are three kinds of tables:
+#   Tables with merged cells
+#   Fancy1 and Facy 2 quotes
+#   Ordinary tables (no merged cells, no "Fancyclopedia 1" or "Fancyclopedia 1" in the first line).  Ordinary tables may or may not have a header row
+def ProcessTable(table):
+
+    # First, check to make sure there are no runs of more than two "|" -- that would indicate a table that can't be handled by
+    # the SimpleTable thingie.
+    # if CheckForAdvancedTable(table):
+    #     print("this is an advanced table")
+    #     return table
+
+    out=[]
+    # The second case is a Fancy 1 or Fancy 2 quote table
+    fancy=CheckForFancyTable(table)
+    if fancy is not None:
+        out.append("{{"+fancy+"|text=")
+        for line in table[1:]:      # We skip the 1st line
+            out.append(GenerateNewTableLine(AnalyzeTableLine(line)))
+        out.append("}}")
+    else:
+        # Generate the output table's first line.
+        # If the input table contains "||~" it's a headerline and will need some special handing in the output.
+        header=AnalyzeTableLine(table[0])
+        headerline="<tab class=wikitable sep=barbar "
+        if "||~" in table[0]:
+            headerline+="head=top "
+        else:
+            headerline+="head= "
+        headerline+="border=1>\n"
+
+        if "||~" in table[0]:
+            if header is not None:
+                for head in header:
+                    headerline+=head+"||"
+                headerline=headerline[:-2]    # We drop the last "||"
+                lines=table[1:]     # If we consume the header line here, remove it.
+        out.append(headerline)
+
+        # Now process the rest of the lines
+        for line in table[1:]:
+            out.append(GenerateNewTableLine(AnalyzeTableLine(line)))
+        out.append("</tab>")
+
+    return out
 
 
 newSite=""
@@ -204,8 +209,9 @@ page="alpha.txt"
 page="apa.txt"
 page="1967.txt"
 page="fanzine.txt"
-page="boskone.txt"
 page="a.txt"
+page="1983-duff-results.txt"
+page="boskone.txt"
 newfile=ProcessFile(os.path.join(oldSite, page))
 with open(os.path.join(newSite, page), "w") as file:
     newfile=[n+"\n" for n in newfile]
